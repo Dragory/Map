32a9b50745f28f00ccd5136d6d2c919af6208458
Miikka Virtanen <dragory.up@gmail.com>
2012-12-27 19:23:54 +0200

    A few changes

--- index.php
+++ index.php
@@ -4,19 +4,14 @@
 <!doctype html>
 <html>
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="UTF-8">
 
-    <title>Map test</title>
+    <title>Map</title>
 
     <link rel="stylesheet" type="text/css" href="css/map.css">
 </head>
 <body>
     <div id="map"></div>
-    <!--<div id="map-viewport">
-        <div id="map-container">
-            <img src="images/mapparts/map.jpg" alt="MAP" style="width: 800px">
-        </div>
-    </div>-->
 
     <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
     <script type="text/javascript" src="js/jquery.mousewheel.js"></script>
@@ -24,10 +19,12 @@
     <script type="text/javascript" src="js/map.js"></script>
     <script type="text/javascript">
         $(document).ready(function() {
-            var map = new Map($('#map'),
-            [
-                'images/mapparts/map.jpg'
-            ]);
+            var $mapElem = $('#map');
+            var map = new Map($mapElem),
+                [
+                    'images/mapparts/map.jpg'
+                ]
+            );
         });
     </script>
 </body>
